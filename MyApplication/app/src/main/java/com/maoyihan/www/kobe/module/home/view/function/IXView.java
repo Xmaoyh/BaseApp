3e02e0369893003e5c8fa6f9567856263a38292d
Xmaoyh <353804616@qq.com>
2017-08-01 17:25:28 +0800

    baseview/basePresenter

--- MyApplication/app/src/main/java/com/maoyihan/www/kobe/module/home/view/function/IXView.java
+++ MyApplication/app/src/main/java/com/maoyihan/www/kobe/module/home/view/function/IXView.java
@@ -1,8 +1,10 @@
 package com.maoyihan.www.kobe.module.home.view.function;
 
+import com.maoyihan.www.kobe.base.IBaseView;
+
 /**
  * Created by owner on 2017/7/5.
  */
 
-public interface IXView {
+public interface IXView extends IBaseView{
 }
